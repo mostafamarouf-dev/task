b1007951f48be8a6cc675f45a0b9d65f6aaf6e47
Your Name <you@example.com>
2024-12-21 22:37:06 +0200

    task js(1)

---
+++ js.js
@@ -0,0 +1,34 @@
+
+function factory(multiplier) {
+    
+    return function (array) {
+        return array.map(num => num * multiplier);
+    };
+}
+
+// Example Usage
+const multiplyByFive = factory(5);
+console.log(multiplyByFive([1, 2, 3])); // [5, 10, 15]
+
+const multiplyByTwo = factory(2);
+console.log(multiplyByTwo([4, 5, 6])); // [8, 10, 12]
+
+// Result By for loop
+
+
+// let a= [1,2,3]
+// var result = []
+// function fact(num) {
+//     for (let index = 0; index < a.length; index++) {
+//         var sum = num * a[index];  
+//         result.push(sum);      
+//     }
+//     return result;
+// }
+
+// var fact5 = fact(5);
+// console.log(fact5);
+
+// var fact4 = fact(4);
+// console.log(fact4);
+
